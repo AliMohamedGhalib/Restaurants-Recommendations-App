0dbebca88eca5b8b1cb08e973f6b42e0250eec27
Ali <alighalib541@gmail.com>
2026-09-25 22:10:48 +0300

    Add create and show routes to restaurant.routes.js

--- routes/restaurant.routes.js
+++ routes/restaurant.routes.js
@@ -11,3 +11,14 @@ router.get("/", async (req, res) => {
 router.get("/new", isSignedIn, (req, res) => {
   res.render("restaurants/new.ejs")
 })
+
+router.post("/", isSignedIn, async (req, res) => {
+  await Restaurant.create({ ...req.body, owner: req.session.user._id })
+  res.redirect("/restaurants")
+})
+
+router.get("/:id", async (req, res) => {
+  const restaurant = await Restaurant.findById(req.params.id)
+  res.render("restaurants/show.ejs", { restaurant })
+})
+
