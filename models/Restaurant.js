f81c7fcf4cbd58dadb1a93a373cb81fdbf150694
Ali <alighalib541@gmail.com>
2026-09-25 21:41:53 +0300

    Rename Restaurant model file and add owner, hasTakeAway, locationLink .

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,18 +1,30 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const restaurantSchema = new mongoose.Schema({
+
     
-    name: {
+       name: {
       type: String,
       required: true,
     },
     hasDineIn: Boolean,
+    hasTakeAway: Boolean,
     hasDriveThrough: Boolean,
+        priceLevel: {
+        type: String,
+        enum: ['2BD and below', 'Above 2BD-4BD', 'Above 4BD-6BD', 'Fine Dining']
+    },
+
+    locationLink: String,
+
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category'
     },
-    priceLevel: Number
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 
 }, {timestamps: true});
 
